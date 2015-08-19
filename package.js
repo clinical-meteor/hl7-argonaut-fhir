92a928ae3ee9ebdfd2b97785012f3be68f18ea2a
Abigail Watson <awatson1978@gmail.com>
2015-08-19 17:41:17 -0500

    moving things around a bit more

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'clinical:hl7-fhir',
-  version: '0.0.1',
+  version: '0.0.3',
   // Brief, one-line summary of the package.
   summary: 'HL7 FHIR for Meteor Apps.  Implements the Argonaut reference spec.',
   // URL to the Git repository containing the source code for this package.
@@ -17,12 +17,12 @@ Package.onUse(function (api) {
   api.versionsFrom('1.1.0.3');
 
   //api.addFiles('lib/mongo.js');
-  api.addFiles('server/initialize.js');
-  api.addFiles('lib/restivus.js');
+  api.addFiles('package/server/initialize.js');
+  api.addFiles('package/lib/restivus.js');
 });
 
 Package.onTest(function (api) {
   api.use('tinytest');
   api.use('clinical:hl7-fhir');
-  api.addFiles('hl7-fhir-tests.js');
+  api.addFiles('package/tests/hl7-fhir-tests.js');
 });
